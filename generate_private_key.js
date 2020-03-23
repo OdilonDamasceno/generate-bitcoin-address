695ba14d9f5b2f6d51036ec7743285b430a6285d
Odilon Damasceno <storckskey@gmail.com>
2020-03-22 23:47:00 -0300

    Create the WIF

--- generate_private_key.js
+++ generate_private_key.js
@@ -1,8 +1,9 @@
 var CryptoJs = require('cryptojs').Crypto;
 
+// The private key is represented for a random numbers of 32 bytes (or 256 bits)
 var privateKey = CryptoJs.util.randomBytes(32);
 
+// Here we change the numbers to hexadecimal numbers
 var privateKeyHex = CryptoJs.util.bytesToHex(privateKey).toUpperCase();
 
-console.log(privateKeyHex);
-
+module.exports = privateKeyHex;
---
+++ generate_wif.js
@@ -0,0 +1,24 @@
+var CryptoJS = require('cryptojs').Crypto;
+var bs58 = require('bs58');
+var privateKey = require('./generate_private_key');
+
+// WIF is represented for a sequence of |1 byte|     32 bytes     | 4 bytes (checksum) |
+// the first byte is the version
+var version = '80';
+
+// the next 32 bytes is your privateKey
+
+var privateKeyVersion = version + privateKey;
+// and the checksum is the validator
+// And how the checksum is created?
+// basically is the first 4 bytes of a Hash of hash of last 33 bytes
+var firstHash = CryptoJS.SHA256(CryptoJS.util.hexToBytes(privateKeyVersion));
+var secondHash = CryptoJS.SHA256(CryptoJS.util.hexToBytes(firstHash));
+var checksum = secondHash.substr(0,8);
+
+var WIF = privateKeyVersion+checksum;
+
+// On the bitcoin network is used the base 58 for represent your address 
+var finalWIF = bs58.encode(CryptoJS.util.hexToBytes(WIF));
+
+console.log(finalWIF);
