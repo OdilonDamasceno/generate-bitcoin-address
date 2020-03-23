a6c95aab6b594abff2d2cc5a379959960f801d8b
Odilon Damasceno <storckskey@gmail.com>
2020-03-23 20:48:54 -0300

    Move codes for src

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var CryptoJS = require('cryptojs').Crypto;
-var bitcoin = require('./generate_bitcoin_address.js');
-var privateKey = require('./generate_private_key.js');
+var bitcoin = require('./src/generate_bitcoin_address.js');
+var privateKey = require('./src/generate_private_key.js');
+var publicKey = require('./src/generate_publicKey');
+var WIF = require('./src/generate_wif');
 const app = express();
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send({
         address: bitcoin,
         privateKey: CryptoJS.util.bytesToHex(privateKey),
+        publicKey: publicKey,
+        wif: WIF,
     })
 });
 
