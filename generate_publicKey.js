639c65b7e46008b14688d34de8f5920e3d0eb176
Odilon Damasceno <storckskey@gmail.com>
2020-03-23 00:40:34 -0300

    Add comments for public Key

--- generate_publicKey.js
+++ generate_publicKey.js
@@ -2,6 +2,9 @@ var CryptoJS = require('cryptojs').Crypto;
 var ec = require('eccrypto');
 var privateKey = require('./generate_private_key.js');
 
+// How the public key is generate.
+// The public key is generated stating from encryptation type
+// eliptics curves. More precisely a secp256k1 curve
 var publicKey = ec.getPublic(Buffer.from(CryptoJS.util.hexToBytes(privateKey)));
 
 module.exports = publicKey;
