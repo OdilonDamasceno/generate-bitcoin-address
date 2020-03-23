ca528ad84e02bc6564c9b262620adb447ee3a024
Odilon Damasceno <storckskey@gmail.com>
2020-03-23 13:08:49 -0300

    Generated final bitcoin address

---
+++ generate_bitcoin_address.js
@@ -0,0 +1,33 @@
+var CryptoJS = require('cryptojs').Crypto;
+var bs58 = require('bs58');
+var bitcoin = require('bitcoinjs-lib');
+var publicKey = require('./generate_publicKey.js');
+
+// Generate the final BTC address 
+// version of bitcoin address. 0x00 (main network), 0x6F (test network), 
+var version = '00';
+
+var publicKeyBytes = CryptoJS.util.hexToBytes(publicKey);
+console.log(publicKeyBytes)
+
+var publicKeySHA256 = CryptoJS.SHA256(publicKeyBytes);
+console.log(publicKeySHA256)
+
+var hash160 = bitcoin.crypto.ripemd160(Buffer.alloc(20,CryptoJS.util.hexToBytes(publicKeySHA256)));
+console.log(hash160);
+
+var hashBytes = Array.prototype.slice.call(hash160, 0);
+hashBytes.unshift(CryptoJS.util.hexToBytes(version));
+console.log(hashBytes);
+
+var firstHash = CryptoJS.SHA256(hashBytes);
+
+var secHash = CryptoJS.SHA256(CryptoJS.util.hexToBytes(firstHash));
+
+var checksum = secHash.substr(0,8);
+
+var address = version + CryptoJS.util.bytesToHex(hash160) + checksum;
+
+var finalAdress = bs58.encode(CryptoJS.util.hexToBytes(address));
+
+console.log(finalAdress);
