fd7580d2011411a25e7565251d5c15fdf82dafb0
Odilon Damasceno <storckskey@gmail.com>
2020-03-23 13:08:10 -0300

    Changed module eccrypto for secp256k1-js

--- generate_private_key.js
+++ generate_private_key.js
@@ -3,7 +3,4 @@ var CryptoJs = require('cryptojs').Crypto;
 // The private key is represented for a random numbers of 32 bytes (or 256 bits)
 var privateKey = CryptoJs.util.randomBytes(32);
 
-// Here we change the numbers to hexadecimal numbers
-var privateKeyHex = CryptoJs.util.bytesToHex(privateKey).toUpperCase();
-
-module.exports = privateKeyHex;
+module.exports = privateKey;
--- generate_publicKey.js
+++ generate_publicKey.js
@@ -1,10 +1,13 @@
 var CryptoJS = require('cryptojs').Crypto;
-var ec = require('eccrypto');
 var privateKey = require('./generate_private_key.js');
+var secp256k1 = require('secp256k1');
 
-// How the public key is generate.
+// How the public key is generated.
 // The public key is generated stating from encryptation type
 // eliptics curves. More precisely a secp256k1 curve
-var publicKey = ec.getPublic(Buffer.from(CryptoJS.util.hexToBytes(privateKey)));
 
-module.exports = publicKey;
+var publicKey = secp256k1.publicKeyCreate(Uint8Array.from(privateKey),false);
+
+var publicKeyHex = CryptoJS.util.bytesToHex(publicKey);
+
+module.exports = publicKeyHex;
