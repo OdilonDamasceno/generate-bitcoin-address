b4a85ce16d2b8930fa52d46840f6be29305cc100
Odilon Damasceno <storckskey@gmail.com>
2020-03-24 20:14:57 -0300

    Generete compressed bitcoin address

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var CryptoJS = require('cryptojs').Crypto;
 var Address = require('./src/generate_bitcoin_address');
-var publicKey = require('./src/generate_publicKey');
+var {publicKeyHex} = require('./src/generate_publicKey');
 var WIF = require('./src/generate_wif');
 var privateKey = require('./src/generate_private_key');
+var compressedAddress = require('./src/generate_bitcoin_address_compressed');
 
 console.log('Your private key is: ' + CryptoJS.util.bytesToHex(privateKey));
 console.log('Your BTC address is: ' + Address);
+console.log('Your compressed BTC address is:', compressedAddress);
 console.log('----------------------------------------------------------------');
 console.log('Your WIF is: ' + WIF);
-console.log('Your public key is: ' + publicKey);
+console.log('Your public key is: ' + publicKeyHex);
--- src/generate_bitcoin_address_compressed.js
+++ src/generate_bitcoin_address_compressed.js
@@ -3,15 +3,13 @@ var bs58 = require('bs58');
 var bitcoin = require('bitcoinjs-lib');
 var publicKey = require('./generate_publicKey.js');
 
-// Generate the final BTC address 
-// version of bitcoin address. 0x00 (main network), 0x6F (test network), 
 var version = '00';
 
-var publicKeyBytes = CryptoJS.util.hexToBytes(publicKey.publicKeyHex);
+var publicKeyCompressedHexBytes = CryptoJS.util.hexToBytes(publicKey.publicKeyCompressedHex);
 
-var publicKeySHA256 = CryptoJS.SHA256(publicKeyBytes);
+var publicKeyCompressedHexSHA256 = CryptoJS.SHA256(publicKeyCompressedHexBytes);
 
-var hash160 = bitcoin.crypto.ripemd160(new Buffer.from(CryptoJS.util.hexToBytes(publicKeySHA256)));
+var hash160 = bitcoin.crypto.ripemd160(new Buffer.from(CryptoJS.util.hexToBytes(publicKeyCompressedHexSHA256)));
 
 var hashBytes = Array.prototype.slice.call(hash160, 0);
 
@@ -25,6 +23,6 @@ var checksum = secHash.substr(0,8);
 
 var address = version + CryptoJS.util.bytesToHex(hash160) + checksum;
 
-var finalAddress = bs58.encode(CryptoJS.util.hexToBytes(address));
+var finalAddressCompressed = bs58.encode(CryptoJS.util.hexToBytes(address));
 
-module.exports = finalAddress;
+module.exports = finalAddressCompressed;
