cd547a288b754505ca00fdb1f267b677300ed03a
Odilon Damasceno <storckskey@gmail.com>
2020-03-23 22:32:12 -0300

    Move archives to src

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var CryptoJS = require('cryptojs').Crypto;
-var Address = require('./generate_bitcoin_address');
-var publicKey = require('./generate_publicKey');
-var WIF = require('./generate_wif');
-var privateKey = require('./generate_private_key');
+var Address = require('./src/generate_bitcoin_address');
+var publicKey = require('./src/generate_publicKey');
+var WIF = require('./src/generate_wif');
+var privateKey = require('./src/generate_private_key');
 
 console.log('Your private key is: ' + CryptoJS.util.bytesToHex(privateKey));
 console.log('Your BTC address is: ' + Address);
